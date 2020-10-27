ecb3ccecc214fbb071419889f432795baf6d0f78
zevon11 <alzahidblack11@gmail.com>
2020-10-27 13:14:56 +0700

    fix bug logic search

--- app/src/main/java/com/fathansatria/pagination3sample/networks/repositories/MovieRepositories.kt
+++ app/src/main/java/com/fathansatria/pagination3sample/networks/repositories/MovieRepositories.kt
@@ -5,14 +5,9 @@ import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import com.fathansatria.pagination3sample.adapters.MoviePagingSource
-import com.fathansatria.pagination3sample.networks.SafeApiRequest
 import com.fathansatria.pagination3sample.models.MovieModel
-import com.fathansatria.pagination3sample.networks.ServiceFactory
 import com.fathansatria.pagination3sample.networks.rests.RestApi
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.launch
-import java.lang.Exception
 
 class MovieRepositories(var service : RestApi) {
 
